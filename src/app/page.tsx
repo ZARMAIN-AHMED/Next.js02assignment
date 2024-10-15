29005d2b257116523f947539e90fc00e42caa5fe
ZARMAIN-AHMED <nazzshagufta604@gmail.com>
2024-10-15 19:38:52 +0500

    assngnment01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 
-import Hero from "./component/hero/page";
-import Navbar from "./component/navbar/page"; // Ensure the correct path and naming convention
-
 import React from 'react';
 
 const MyName = () => {
   return (
-    <div>
-        <div>
-             <Navbar />
-            <Hero/> </div> 
-    </div>
+   <div>
+    <h1 className='bg-red-400 h-auto w-60 text-center mt-52  ml-80 font-semibold text-4xl rounded-lg p-4'>HELLO WORLD!</h1><br/>
+    <h1 className=' bg-amber-700 h-64 w-60 text-center ml-80 font-extrabold text-4xl rounded-lg p-4'>WELCOME TO NEXT.JS WORLD</h1>
+   </div>
   );
 }
 
